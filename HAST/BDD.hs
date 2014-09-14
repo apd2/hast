649ff1a077c5a06a94385aaa1d85896c0048c3d7
Adam Walker <adamwalker10@gmail.com>
2014-09-14 18:01:47 -0400

    resource monad in compilation WIP

--- HAST/BDD.hs
+++ HAST/BDD.hs
@@ -1,84 +1,121 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, TemplateHaskell, FlexibleContexts, ScopedTypeVariables, ConstraintKinds #-}
 
-module HAST.BDD(compileBDD) where
+module HAST.BDD(compileBDD, compileBDD') where
 
 import Control.Monad.State
 import Control.Monad.ST
 import Data.Bits
 import Debug.Trace
+import Control.Monad.Morph
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
 
 import Util hiding (trace)
 import Cudd.Imperative
 import Synthesis.Interface
+import Synthesis.Resource
+import Synthesis.RefineCommon
 import HAST.HAST
 
-block :: (STDdManager s u -> DDNode s u -> DDNode s u -> ST s (DDNode s u)) -> (STDdManager s u -> DDNode s u) -> STDdManager s u -> [DDNode s u] -> ST s (DDNode s u)
+block :: (RM s u t) => 
+         (STDdManager s u -> DDNode s u -> DDNode s u -> ST s (DDNode s u)) -> 
+         (STDdManager s u -> DDNode s u) -> 
+         STDdManager s u -> 
+         [DDNode s u] -> 
+         t (ST s) (DDNode s u)
 block func s m nodes = do
-    ref (s m)
+    $rp ref (s m)
     go (s m) nodes
     where
     go accum []     = return accum
     go accum (n:ns) = do
-        accum' <- func m accum n
-        deref m accum
-        deref m n
+        accum' <- $r2 (func m) accum n
+        $d (deref m) accum
+        $d (deref m) n
         go accum' ns
 
+conj, disj :: (RM s u t) 
+           => STDdManager s u 
+           -> [DDNode s u] 
+           -> t (ST s) (DDNode s u)
 conj = block band bone
 disj = block bor  bzero
 
-ccase :: STDdManager s u -> [(DDNode s u, DDNode s u)] -> ST s (DDNode s u)
+ccase :: (RM s u t) => STDdManager s u -> [(DDNode s u, DDNode s u)] -> t (ST s) (DDNode s u)
 ccase m = go (bzero m) (bzero m)
     where
     go accum neg [] = do
-        deref m neg
+        $d (deref m) neg
         return accum
     go accum neg ((cond, cas): cs) = do
         --alive == cond, cas, accum, neg
-        econd  <- band m cond (bnot neg)
-        clause <- band m econd cas
-        deref m econd
-        deref m cas
+        econd  <- $r2 (band m) cond (bnot neg)
+        clause <- $r2 (band m) econd cas
+        $d (deref m) econd
+        $d (deref m) cas
         --alive == cond, accum, neg, clause
-        accum' <- bor m clause accum
-        deref m accum
-        deref m clause
+        accum' <- $r2 (bor m) clause accum
+        $d (deref m) accum
+        $d (deref m) clause
         --alive == cond, neg, accum'
-        neg' <- bor m cond neg
-        deref m cond
-        deref m neg
+        neg' <- $r2 (bor m) cond neg
+        $d (deref m) cond
+        $d (deref m) neg
         --alive == accum', neg'
         go accum' neg' cs
 
+compileBDD' :: forall v s u pdb. (Show v) 
+           => STDdManager s u 
+           -> VarOps pdb v s u 
+           -> (v -> Maybe String)                          -- returns BDD variable group tag for a variable
+           -> AST [DDNode s u] [DDNode s u] (DDNode s u) v 
+           -> StateT pdb (ST s) (DDNode s u)
+compileBDD' m vo ft ast = hoist (liftM fst . (runResource :: Monad m => InUse (DDNode s u) -> (ResourceT (DDNode s u)) m a -> m (a, InUse (DDNode s u))) Map.empty) $ (compileBDD m vo ft ast :: StateT pdb (ResourceT (DDNode s u) (ST s)) (DDNode s u))
 
-
-compileBDD :: (Show v) => STDdManager s u 
-                       -> VarOps pdb v s u 
-                       -> (v -> Maybe String)                          -- returns BDD variable group tag for a variable
-                       -> AST [DDNode s u] [DDNode s u] (DDNode s u) v 
-                       -> StateT pdb (ST s) (DDNode s u)
+compileBDD :: forall v s u t pdb. (Show v, RM s u t) 
+           => STDdManager s u 
+           -> VarOps pdb v s u 
+           -> (v -> Maybe String)                          -- returns BDD variable group tag for a variable
+           -> AST [DDNode s u] [DDNode s u] (DDNode s u) v 
+           -> StateT pdb (t (ST s)) (DDNode s u)
 compileBDD m VarOps{..} ftag = compile' where
+
+    binOp :: forall t. RM s u t => (STDdManager s u -> DDNode s u -> DDNode s u -> ST s (DDNode s u))
+          -> STDdManager s u 
+          -> AST [DDNode s u] [DDNode s u] (DDNode s u) v
+          -> AST [DDNode s u] [DDNode s u] (DDNode s u) v
+          -> StateT pdb (t (ST s)) (DDNode s u)
     binOp func m x y = do
         x <- compile' x
         y <- compile' y
-        res <- lift $ func m x y 
-        lift $ deref m x
-        lift $ deref m y
+        res <- lift $ $r2 (func m) x y 
+        lift $ $d (deref m) x
+        lift $ $d (deref m) y
         return res
 
+    getAVar :: forall t. RM s u t => ASTVar [DDNode s u] [DDNode s u] v -> StateT pdb (t (ST s)) [DDNode s u]
     getAVar (FVar f)       = return f
     getAVar (EVar e)       = return e
-    getAVar (NVar v)       = getVar v (ftag v)
+    getAVar (NVar v)       = hoist lift $ getVar v (ftag v) --TODO fix when Interface.hs uses resource monad
 
-    withTmpMany n f = withTmpMany' [] n f
-    withTmpMany' nodes 0 f = f nodes
+    withTmpMany :: forall t. RM s u t => Int -> ([DDNode s u] -> StateT pdb ((ResourceT (DDNode s u)) (ST s)) (DDNode s u)) -> StateT pdb (t (ST s)) (DDNode s u)
+    withTmpMany n f = do
+        (res :: DDNode s u) <- hoist lift $ withTmpMany' [] n f
+        lift $ $rr $ return res
+        return res
+
+    withTmpMany' :: [DDNode s u] -> Int -> ([DDNode s u] -> StateT pdb ((ResourceT (DDNode s u)) (ST s)) (DDNode s u)) -> StateT pdb (ST s) (DDNode s u)
+    withTmpMany' nodes 0 f = hoist (liftM fst . runResource Map.empty) $ f nodes
     withTmpMany' nodes n f = withTmp (\node -> withTmpMany' (node:nodes) (n-1) f)
 
+    compile' :: forall t. RM s u t => AST [DDNode s u] [DDNode s u] (DDNode s u) v 
+             -> StateT pdb (t (ST s)) (DDNode s u)
+
     compile' T             = do
-        lift $ ref $ bone m
+        lift $ $rp ref $ (bone m :: DDNode s u)
         return $ bone m
     compile' F             = do
-        lift $ ref $ bzero m
+        lift $ $rp ref $ (bzero m :: DDNode s u)
         return $ bzero m
     compile' (Not x)       = liftM bnot $ compile' x
     compile' (And x y)     = binOp band m x y
@@ -104,43 +141,37 @@ compileBDD m VarOps{..} ftag = compile' where
     compile' (EqVar x y)   = do
         x <- getAVar x
         y <- getAVar y
-        lift $ xeqy m x y
+        lift $ $r $ xeqy m x y --TODO reference counting
     compile' (EqConst x c) = do
         x <- getAVar x
-        lift $ computeCube m x $ bitsToBoolArrBe (length x) c
-
-    compile' (Exists w f) | w <= 0    = error $ "compileBDD error: cannot quantify " ++ show w ++ " bits"
-                          | otherwise = withTmpMany w $ \x -> do
-        res' <- compile' $ f x
---        lift $ foldM (\r v -> do res <- bexists m r v
---                                 deref m r
---                                 return res) res' x
-        xcube <- lift $ nodesToCube m x
-        res  <- lift $ bexists m res' xcube
-        lift $ deref m xcube
-        lift $ deref m res'
-        return res
-    compile' (NExists n w f) | w <= 0    = error $ "compileBDD error: cannot quantify " ++ show w ++ " bits"
-                             | otherwise = withTmpMany w $ \x -> do
-        res' <- compile' $ f x
-        xcube <- lift $ nodesToCube m x
-        --sz'  <- lift $ dagSize res'
-        --sup  <- lift $ supportIndices m res'
-        res  <- lift $ {-trace ("quantifying " ++ show n ++ " dagsize " ++ show sz' ++ " support " ++ (show (length sup))) $-} bexists m res' xcube
-        --sz   <- lift $ dagSize res
-        --trace ("dagsize after quantification " ++ show sz) $ return ()
-        lift $ deref m xcube
-        lift $ deref m res'
-        return res
+        lift $ $r $ computeCube m x $ bitsToBoolArrBe (length x) c --TODO reference counting
+    compile' (Exists w f) 
+        | w <= 0    = error $ "compileBDD error: cannot quantify " ++ show w ++ " bits"
+        | otherwise = withTmpMany w $ \x -> do
+            res' <- compile' $ f x
+            xcube <- lift $ $r $ nodesToCube m x --TODO reference counting
+            res  <- lift $ $r2 (bexists m) res' xcube
+            lift $ $d (deref m) xcube
+            lift $ $d (deref m) res'
+            return res
+    compile' (NExists n w f) 
+        | w <= 0    = error $ "compileBDD error: cannot quantify " ++ show w ++ " bits"
+        | otherwise = withTmpMany w $ \x -> do
+            res' <- compile' $ f x
+            xcube <- lift $ $r $ nodesToCube m x --TODO reference counting
+            res  <- lift $ $r2 (bexists m) res' xcube
+            lift $ $d (deref m) xcube
+            lift $ $d (deref m) res'
+            return res
     compile' (Var x)       = do
         [x] <- getAVar x
-        lift $ ref x
+        lift $ $rp ref x
         return x
     compile' (Let x f)     = do
         bind <- compile' x
         res  <- compile' (f bind)
-        lift $ deref m bind
+        lift $ $d (deref m) bind
         return res
     compile' (LetLit x)    = do
-        lift $ ref x
+        lift $ $rp ref x
         return x
